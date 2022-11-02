66ca8b980686639ecf27c33de4e23fa5fea5c9eb
Raphy <gohanpeixe@gmail.com>
2022-11-02 10:50:55 -0400

    First

--- main.py
+++ main.py
@@ -13,16 +13,22 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 }
 
-
 lastPosted = dict()
 
 def main():
 
     for (idx, link) in enumerate(settings['links']):
 
+        posted = False
+
         today = datetime.datetime.now().strftime("%Y-%m-%d")
         print(f'Opening link {idx+1} of {len(settings["links"])}')
-        response = requests.get(link, headers= headers)
+        
+        try:
+            response = requests.get(link, headers= headers)
+        except:
+            print('Error opening link')
+            continue
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -36,11 +42,16 @@ def main():
         if soup.find("span", class_= "coming-soon-label"):
             print('Coming Soon detected not posting to discord.')
             availability = soup.find('span', class_= 'coming-soon-label').text.strip().lower()
+            time.sleep(10)
+            print('Continuing...\n')
+            continue
         
         elif soup.find('span', class_= 'stock-level product-in-stock'):
             availability = soup.find('span', class_= 'stock-level product-in-stock').text.strip().lower()
+            posted = True
 
         if "in stock" in availability:
+
             availability = "In Stock"
 
             if lastPosted.get(link, None):
@@ -52,10 +63,9 @@ def main():
                 else:
                     lastPosted[link] = today
                     print('Available! posting to discord')
-                    print(image)
-                    webhook = DiscordWebhook(url=settings['discordWebhookUrl'])
+                    webhook = DiscordWebhook(url=settings['discordWebhookUrl'], content= settings['discordUserId'])
                     embed = DiscordEmbed(title=title, color=0x00FF00)
-                    embed.set_url= link
+                    embed.set_url(url= link)
                     embed.set_thumbnail(url=image)
                     embed.add_embed_field(name='Price', value=price, inline= False)
                     embed.add_embed_field(name='Availability', value=availability, inline= False)
@@ -69,10 +79,9 @@ def main():
             else:
                 lastPosted[link] = today
                 print('Available! posting to discord')
-                print(image)
-                webhook = DiscordWebhook(url=settings['discordWebhookUrl'])
+                webhook = DiscordWebhook(url=settings['discordWebhookUrl'], content= settings['discordUserId'])
                 embed = DiscordEmbed(title=title, color=0x00FF00)
-                embed.set_url= link
+                embed.set_url(url= link)
                 embed.set_thumbnail(url=image)
                 embed.add_embed_field(name='Price', value=price, inline= False)
                 embed.add_embed_field(name='Availability', value=availability, inline= False)
@@ -83,7 +92,60 @@ def main():
                 webhook.add_embed(embed)
                 response = webhook.execute()
                 print('Posted to discord')
+
+        #Check if the add to cart button is there
+
+        if not posted:
+            
+            if soup.find('div', class_= 'add-button-wrapper widget-fingerprint-product-add-button'):
+                availability = "In Stock"
+
+            if lastPosted.get(link, None):
+
+                if lastPosted[link] == today:
+                    print()
+                    print('Already posted today, not posting again. until tomorrow')
+                    time.sleep(20)
+                    continue
+                
+                else:
+                        lastPosted[link] = today
+                        print('Available! posting to discord')
+        
+                        webhook = DiscordWebhook(url=settings['discordWebhookUrl'], content= settings['discordUserId'])
+                        embed = DiscordEmbed(title=title, color=0x00FF00)
+                        embed.set_url(url= link)
+                        embed.set_thumbnail(url=image)
+                        embed.add_embed_field(name='Price', value=price, inline= False)
+                        embed.add_embed_field(name='Availability', value=availability, inline= False)
+                        embed.add_embed_field(name= 'Link', value=f'[Click Here]({link})', inline= False)
+                        embed.set_footer(text='Ozbongs Monitor', icon_url='https://i.imgur.com/8K1ZiKA.png')
+                        embed.set_author(name='Ozbongs Monitor', url=link, icon_url='https://i.imgur.com/8K1ZiKA.png')
+                        embed.set_timestamp()
+                        webhook.add_embed(embed)
+                        response = webhook.execute()
+                        print('Posted to discord')
+            else:
+                lastPosted[link] = today
+                print('Available! posting to discord')
+                webhook = DiscordWebhook(url=settings['discordWebhookUrl'], content= settings['discordUserId'])
+                embed = DiscordEmbed(title=title, color=0x00FF00)
+                embed.set_url(url= link)
+                embed.set_thumbnail(url=image)
+                embed.add_embed_field(name='Price', value=price, inline= False)
+                embed.add_embed_field(name='Availability', value=availability, inline= False)
+                embed.add_embed_field(name= 'Link', value=f'[Click Here]({link})', inline= False)
+                embed.set_footer(text='Ozbongs Monitor', icon_url='https://i.imgur.com/8K1ZiKA.png')
+                embed.set_author(name='Ozbongs Monitor', url=link, icon_url='https://i.imgur.com/8K1ZiKA.png')
+                embed.set_timestamp()
+                webhook.add_embed(embed)
+                response = webhook.execute()
+                print('Posted to discord')
+
+        
         time.sleep(20)
+
+
 
     print('Sleeping for 1 minute')
 
--- readme.md
+++ readme.md
@@ -7,8 +7,15 @@
 ### Installation
 
 1. Unzip the file
-2. Open a terminal in the folder
-3. Run `pip install -r requirements.txt`
-4. Make changes to the settings.json file.
-4. Run `python main.py` or `python3 main.py` or `py main.py`
+2. Open a terminal in the folder. If you're on Mac, open terminal and type `cd ` and drag the folder into the terminal window. Press enter.
+3. Run `python3 -m pip install -r requirements.txt`
+4. Make changes to the settings.json file if needed.
+4. Run `python3 main.py`
 
+### How to add or remove links
+
+1. Make sure to close the app before adding or removing links.
+2. Open the settings.json file
+3. Add or remove links from the "links" array
+4. Save the file
+5. Run the program again
